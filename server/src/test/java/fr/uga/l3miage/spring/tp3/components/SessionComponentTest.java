bf7acce2ea9fed8b4cccbfebdf88fca2c8a9b86f
Laptop House <xzedrez@gmail.com>
2024-04-06 14:49:29 +0200

    ajout session component

--- server/src/test/java/fr/uga/l3miage/spring/tp3/components/SessionComponentTest.java
+++ server/src/test/java/fr/uga/l3miage/spring/tp3/components/SessionComponentTest.java
@@ -1,4 +1,68 @@
 package fr.uga.l3miage.spring.tp3.components;
 
+import fr.uga.l3miage.spring.tp3.exceptions.rest.CreationSessionRestException;
+import fr.uga.l3miage.spring.tp3.models.CandidateEntity;
+import fr.uga.l3miage.spring.tp3.models.EcosSessionEntity;
+import fr.uga.l3miage.spring.tp3.models.EcosSessionProgrammationEntity;
+import fr.uga.l3miage.spring.tp3.models.EcosSessionProgrammationStepEntity;
+import fr.uga.l3miage.spring.tp3.repositories.EcosSessionRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.Optional;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+
+@AutoConfigureTestDatabase
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 public class SessionComponentTest {
+    @Autowired
+    private SessionComponent sessionComponent;
+
+    @MockBean
+    private EcosSessionRepository ecosSessionRepository;
+
+    @Test
+    void testCreateSessionNotFound(){
+        // given
+        when(ecosSessionRepository.findById(anyLong())).thenReturn(Optional.empty());
+        //then - when
+        assertThrows(NullPointerException.class, ()->sessionComponent.createSession(new EcosSessionEntity()));
+    }
+
+    @Test
+    void testCreateSessionFound(){
+        //Given
+
+        EcosSessionProgrammationStepEntity ecosSessionProgrammationStepEntity = EcosSessionProgrammationStepEntity
+                .builder()
+                .code("101")
+                .description("Vas-y mec")
+                .build();
+
+        EcosSessionProgrammationEntity ecosSessionProgrammationEntity = EcosSessionProgrammationEntity
+                .builder()
+                .label("Label")
+                .ecosSessionProgrammationStepEntities(Set.of(ecosSessionProgrammationStepEntity))
+                .build();
+
+
+        EcosSessionEntity ecosSessionEntity = EcosSessionEntity
+                .builder()
+                .name("Session 1")
+                .ecosSessionProgrammationEntity(ecosSessionProgrammationEntity)
+                .build();
+
+        // given
+        when(ecosSessionRepository.findById(anyLong())).thenReturn(Optional.of(ecosSessionEntity));
+        //when - then
+        assertDoesNotThrow(()->sessionComponent.createSession(ecosSessionEntity));
+    }
 }
--- server/src/test/java/fr/uga/l3miage/spring/tp3/services/SessionServiceTest.java
+++ server/src/test/java/fr/uga/l3miage/spring/tp3/services/SessionServiceTest.java
@@ -1,4 +1,28 @@
 package fr.uga.l3miage.spring.tp3.services;
 
+import fr.uga.l3miage.spring.tp3.components.CandidateComponent;
+import fr.uga.l3miage.spring.tp3.components.SessionComponent;
+import fr.uga.l3miage.spring.tp3.exceptions.rest.CreationSessionRestException;
+import fr.uga.l3miage.spring.tp3.exceptions.technical.CandidateNotFoundException;
+import fr.uga.l3miage.spring.tp3.repositories.CandidateRepository;
+import fr.uga.l3miage.spring.tp3.repositories.EcosSessionRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+
+@AutoConfigureTestDatabase
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 public class SessionServiceTest {
+
+
 }
+
+
